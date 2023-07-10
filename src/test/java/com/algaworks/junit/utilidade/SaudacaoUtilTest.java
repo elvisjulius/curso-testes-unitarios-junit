e2d5dbb865ebb73e4a5c052719eded99f53c4989
Elvis <elvisjulius@hotmail.com>
2023-07-09 21:07:36 -0300

    2.3. Explorando as asserções

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,15 +1,14 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class SaudacaoUtilTest {
 
     @Test
     public void saudar() {
         String saudacao = SaudacaoUtil.saudar(9);
-        Assertions.assertTrue(saudacao.equals("Bom dia"));
+        assertEquals("Bom dia", saudacao);
     }
 }
