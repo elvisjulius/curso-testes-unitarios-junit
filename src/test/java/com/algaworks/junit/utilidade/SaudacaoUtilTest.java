2a3c252619e9b11b4ecfbae2a09b1574a354968e
Elvis <elvisjulius@hotmail.com>
2023-07-09 21:17:29 -0300

    2.4. Asserções de Exceptions

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,7 +1,8 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class SaudacaoUtilTest {
@@ -10,5 +11,18 @@ class SaudacaoUtilTest {
     public void saudar() {
         String saudacao = SaudacaoUtil.saudar(9);
         assertEquals("Bom dia", saudacao);
+    }
+
+    @Test
+    public void deveLancarException(){
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> SaudacaoUtil.saudar(-10));
+
+        assertEquals("Hora inválida", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    public void naoDeveLancarException(){
+        assertDoesNotThrow(()-> SaudacaoUtil.saudar(0));
     }
 }
