cb34c478e3d58f21497e2c2b5e2668afbc278ef0
Elvis <elvisjulius@hotmail.com>
2023-07-10 08:03:13 -0300

    2.5. Asserções em listas

---
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -0,0 +1,28 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiltroNumerosTest {
+
+    @Test
+    public void deveRetornarNumerosPares() {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+        List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
+        List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
+        Assertions.assertIterableEquals(numerosParesEsperados,resultadoFiltro);
+    }
+
+    @Test
+    void numerosImpares() {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
+        List<Integer> numerosImparesEsperados = Arrays.asList(1, 3);
+        List<Integer> resultadoFiltro = FiltroNumeros.numerosImpares(numeros);
+        Assertions.assertIterableEquals(numerosImparesEsperados,resultadoFiltro);
+    }
+}
