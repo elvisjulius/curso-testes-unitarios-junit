ad2476545898af5e77e7319082397c5ff6cd5b44
Elvis <elvisjulius@hotmail.com>
2023-07-22 20:41:08 -0300

    3.5. Organizando classe de testes com sub-classes e @Nested

--- src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.awt.event.ContainerAdapter;
@@ -10,70 +11,81 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ContaBancariaTest {
 
-    @Test
-    void saqueNaoRealizado() {
+    @Nested
+    class Saque{
+        @Test
+        void saqueNaoRealizado() {
 
-        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> contaBancaria.saque(new BigDecimal(0)));
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.saque(new BigDecimal(0)));
 
-        assertEquals("O valor não pode ser nulo ou igual ou menor que 0", illegalArgumentException.getMessage());
+            assertEquals("O valor não pode ser nulo ou igual ou menor que 0", illegalArgumentException.getMessage());
 
+        }
+
+        @Test
+        void saqueNaoPodeSerMaiorQueSaldo() {
+
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
+            RuntimeException runtimeException = assertThrows(RuntimeException.class,
+                    () -> contaBancaria.saque(new BigDecimal(1001)));
+
+            assertEquals("Saldo insuficiente", runtimeException.getMessage());
+
+        }
+
+        @Test
+        void saqueRealizadoMudaSaldo() {
+
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
+
+            contaBancaria.saque(new BigDecimal(499));
+            assertEquals(new BigDecimal(501), contaBancaria.saldo());
+
+        }
     }
 
-    @Test
-    void saqueNaoPodeSerMaiorQueSaldo() {
+    @Nested
+    class Deposito{
 
-        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
-        RuntimeException runtimeException = assertThrows(RuntimeException.class,
-                () -> contaBancaria.saque(new BigDecimal(1001)));
+        @Test
+        void depositoNaoPodeSerNuloZeroOuMenorQueZero() {
 
-        assertEquals("Saldo insuficiente", runtimeException.getMessage());
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.deposito(new BigDecimal(0)));
 
+            assertEquals("O valor não pode ser nulo ou igual ou menor que 0", illegalArgumentException.getMessage());
+        }
+
+        @Test
+        void depositoRealizadoMudaSaldo() {
+
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
+
+            contaBancaria.deposito(new BigDecimal(499));
+            assertEquals(new BigDecimal(1499), contaBancaria.saldo());
+
+        }
     }
 
-    @Test
-    void saqueRealizadoMudaSaldo() {
+    @Nested
+    class Saldo{
 
-        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
+        @Test
+        void saldoNaoPodeSerNulo() {
+            assertThrows(IllegalArgumentException.class, () -> {
+                new ContaBancaria(null);
+            }, "O Saldo não pode ser nulo");
+        }
 
-        contaBancaria.saque(new BigDecimal(499));
-        assertEquals(new BigDecimal(501), contaBancaria.saldo());
+        @Test
+        void podeSerZeroOuNegativo() {
 
-    }
+            ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal("-10.5"));
 
-    @Test
-    void depositoNaoPodeSerNuloZeroOuMenorQueZero() {
-
-        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> contaBancaria.deposito(new BigDecimal(0)));
-
-        assertEquals("O valor não pode ser nulo ou igual ou menor que 0", illegalArgumentException.getMessage());
-    }
-
-    @Test
-    void depositoRealizadoMudaSaldo() {
-
-        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(1000));
-
-        contaBancaria.deposito(new BigDecimal(499));
-        assertEquals(new BigDecimal(1499), contaBancaria.saldo());
-
-    }
-
-    @Test
-    void saldoNaoPodeSerNulo() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            new ContaBancaria(null);
-        }, "O Saldo não pode ser nulo");
-    }
-
-    @Test
-    void podeSerZeroOuNegativo() {
-
-        ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal("-10.5"));
-
-        assertEquals(new BigDecimal("-10.5"), contaBancaria.saldo());
+            assertEquals(new BigDecimal("-10.5"), contaBancaria.saldo());
+        }
     }
 }
