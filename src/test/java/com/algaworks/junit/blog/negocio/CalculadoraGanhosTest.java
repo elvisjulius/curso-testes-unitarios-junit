4e179d02d39b42216f272621b13c05e7e5b4ade3
Elvis <elvisjulius@hotmail.com>
2023-07-22 21:58:23 -0300

    3.7. Um teste deve ter uma única asserção?

--- src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CalculadoraGanhosTest.java
@@ -10,6 +10,8 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class CalculadoraGanhosTest {
 
     static CalculadoraGanhos calculadora;
@@ -30,23 +32,37 @@ class CalculadoraGanhosTest {
     }
 
     @Test
-    void deveCalcularGanhos() {
+    void Dado_um_post_e_autor_premium_Quando_calcular_ganhos_Entao_deve_retornar_valor_com_bonus() {
 
         Ganhos ganhos = calculadora.calcular(post);
 
         assertEquals(new BigDecimal("45"),ganhos.getTotalGanho());
+    }
+
+    @Test
+    void Dado_um_post_e_autor_premium_Quando_calcular_ganhos_Entao_deve_retornar_quantidade_de_palavras_no_post() {
+
+        Ganhos ganhos = calculadora.calcular(post);
+
+
         assertEquals(7,ganhos.getQuantidadePalavras());
+
+    }
+
+    @Test
+    void Dado_um_post_e_autor_premium_Quando_calcular_ganhos_Entao_deve_retornar_valor_pago_por_palavra_do_autor() {
+
+        Ganhos ganhos = calculadora.calcular(post);
+
         assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
     }
 
     @Test
-    void deveCalcularGanhosSemPremium() {
+    void Dado_um_post_e_autor_sem_premium_Quando_calcular_ganhos_Entao_deve_retornar_valor_com_bonus() {
         autor.setPremium(false);
 
         Ganhos ganhos = calculadora.calcular(post);
 
         assertEquals(new BigDecimal("35"),ganhos.getTotalGanho());
-        assertEquals(7,ganhos.getQuantidadePalavras());
-        assertEquals(autor.getValorPagoPorPalavra(), ganhos.getValorPagoPorPalavra());
     }
 }
